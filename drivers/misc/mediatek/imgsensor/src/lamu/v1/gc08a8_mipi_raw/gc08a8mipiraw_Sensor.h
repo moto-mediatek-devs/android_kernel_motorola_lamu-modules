501d456005bb6f0fd5424d4821457fbd9eab12d5
yuanyang <yang.yuan@tinno.com>
2024-07-11 15:40:21 +0800

    <BUG><TEACAA-1453><[LAMU][CAMERA]Change GC08A8 Camera Orientation>
    Change-Id: I03ba8a71e8e9c084ada56b70144bfa9f08aab272

--- drivers/misc/mediatek/imgsensor/src/lamu/v1/gc08a8_mipi_raw/gc08a8mipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/lamu/v1/gc08a8_mipi_raw/gc08a8mipiraw_Sensor.h
@@ -38,10 +38,10 @@
 
 
 /* SENSOR MIRROR FLIP INFO */
-#define GC08A8_MIRROR_NORMAL    0
+#define GC08A8_MIRROR_NORMAL    1
 #define GC08A8_MIRROR_H         0
 #define GC08A8_MIRROR_V         0
-#define GC08A8_MIRROR_HV        1
+#define GC08A8_MIRROR_HV        0
 
 #if GC08A8_MIRROR_NORMAL
 #define GC08A8_MIRROR	        0x00
