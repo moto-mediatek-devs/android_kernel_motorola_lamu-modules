6d45c1bf3b45c623439f1bbfcd4d7260ae5850f3
朱雅婷 <yating.zhu@tinno.com>
2024-07-17 20:54:08 +0800

    Revert "Revert "<REQ><EKLAMU-1442><lamu lite add td4160 tp >""
    This reverts commit 72e4057c0c5e69050e6b15987f8a6cb00161d802.
    
    Reason for revert: <lamu lite add td4160 tp>
    
    Change-Id: Ib7fec934ba05ae1b90674707216ddbe6bf328ed0

--- drivers/gpu/drm/panel/panel-tm-td4376-vdo.c
+++ drivers/gpu/drm/panel/panel-tm-td4376-vdo.c
@@ -43,6 +43,9 @@
 int gesture_mode = -1;
 #endif
 
+int td4376_lcd_id = 0;
+EXPORT_SYMBOL(td4376_lcd_id);
+
 int hbm;
 bool is_hbm;
 bool is_suspend;
@@ -1060,6 +1063,8 @@ static int td4376_probe(struct mipi_dsi_device *dsi)
 
 	ptx = ctx;
 	hbm = 0;
+
+	td4376_lcd_id = 0x010d;
 
 	pr_info("td4376 %s --- end\n", __func__);
 
