32f34dd7043abdd051a93ca7c5020c957b3b51ed
Jerry <jirui.li@tinno.com>
2024-09-13 17:11:08 +0800

    EKLAMU-202: update charging icons fast when set enable_charger/enable_hiz
    Change-Id: I718a0f0256c8f7a7f98326490de134f962097105
    Reviewed-on: https://gerrit-odm.mot.com/c/home/repo/dev/platform/android/quark/kernel_device_modules-6.6/+/7899
    Tested-by: jirui li <jirui.li@tinno.com>
    Tested-by: PDT SW Engineering <pdtsweng@motorola.com>
    Reviewed-by: hao jia <hao.jia@tinno.com>
    Tested-by: hao jia <hao.jia@tinno.com>
    Reviewed-by: jirui li <jirui.li@tinno.com>
    Tested-by: gerrito

--- drivers/power/supply/mtk_charger.c
+++ drivers/power/supply/mtk_charger.c
@@ -1811,6 +1811,7 @@ static ssize_t enable_hiz_store(struct device *dev,
 		} else {
 			pinfo->enable_hiz = false;
 		}
+		_wake_up_charger(pinfo);
 		charger_dev_enable_hz(pinfo->chg1_dev, pinfo->enable_hiz);
 	} else {
 		chr_err("%s: format error!\n", __func__);
@@ -1845,6 +1846,7 @@ static ssize_t enable_charger_store(struct device *dev,
 		} else {
 			pinfo->enable_charger = false;
 		}
+		_wake_up_charger(pinfo);
 		charger_dev_enable(pinfo->chg1_dev, pinfo->enable_charger);
 	} else {
 		chr_err("%s: format error!\n", __func__);
