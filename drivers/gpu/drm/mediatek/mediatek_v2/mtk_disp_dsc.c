77e2ae5c5298bc504d25b0a0494fa369464fab1b
rio004 <riyon336@gmail.com>
2026-04-20 10:41:11 +0000

    drivers: Guard (MIN|MAX) macros
    Change-Id: Ibc3dee7e4cf89bed47a068b3036cf8240f781918
    Signed-off-by: rio004 <riyon336@gmail.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_dsc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_dsc.c
@@ -30,7 +30,9 @@
 #endif
 #include "mtk_disp_dsc.h"
 #include "platform/mtk_drm_platform.h"
+#ifndef MIN
 #define MIN(X, Y) ((X) <= (Y) ? (X) : (Y))
+#endif
 
 #define DISP_REG_DSC_CON			0x0000
 	#define DSC_EN BIT(0)
