77f6a4227b511d2d856982e178e1fca6cf652afb
R0rt1z2 <roger@r0rt1z2.com>
2026-07-06 20:23:54 +0200

    misc: mediatek: cmdq: Rate-limit cmdq_msg and cmdq_err
    These print unconditionally at notice level and spam the log
    (e.g. "cl/dev is null" every frame). Use the ratelimited variants.
    
    Change-Id: I1f1c3b797c773624e8fa298bf3d3767b878486e5

--- include/linux/mailbox/mtk-cmdq-mailbox-ext.h
+++ include/linux/mailbox/mtk-cmdq-mailbox-ext.h
@@ -321,10 +321,10 @@ do { \
 	pr_notice("[cmdq] "fmt" @%s,%u\n", ##args, __func__, __LINE__)
 
 #define cmdq_msg(fmt, args...) \
-	pr_notice("[cmdq] "fmt"\n", ##args)
+	pr_notice_ratelimited("[cmdq] "fmt"\n", ##args)
 
 #define cmdq_err(fmt, args...) \
-	pr_notice("[cmdq][err] "fmt" @%s,%u\n", ##args, __func__, __LINE__)
+	pr_notice_ratelimited("[cmdq][err] "fmt" @%s,%u\n", ##args, __func__, __LINE__)
 
 #define cmdq_dump(fmt, args...) \
 	pr_notice("[cmdq][err] "fmt"\n", ##args)
